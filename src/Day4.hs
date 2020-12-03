8af85dbae33d6b926975ea7bdb7f41dce0ffe9e8
Andreas Källberg <anka.213@gmail.com>
2020-12-03 21:32:04 +0100

    Tweaks on template

--- src/Day4.hs
+++ src/Day4.hs
@@ -18,10 +18,9 @@ day4 :: Day Day4Data
 day4 = Day
     { dayNr = 4
     , parser = parseDay4
-    , part1 = undefined
-    , part2 = undefined
+    , part1 = day4part1
+    , part2 = day4part2
     }
-
 
 parseDay4 :: String -> Day4Data
 parseDay4 = fmap parseLine . lines
@@ -32,13 +31,19 @@ parseLine _ = ()
 
 -- >>>  getSampleInput day4
 
--- >>> foo <$> getSampleInput day3
+-- >>> foo <$> getSampleInput day4
+
+day4part1 :: Day4Data -> Output
+day4part1 = error "not implemented"
 
 -- >>> runDay day4 Part1 Sample
 
 -- >>> runDay day4 Part1 Real
 
 -- * Part 2
+
+day4part2 :: Day4Data -> Output
+day4part2 = error "not implemented"
 
 -- >>> runDay day4 Part2 Sample
 
